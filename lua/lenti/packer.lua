356834fbcb37141898bc51180e93fbef7641cf7a
florent-haxhiu <florent_h2@hotmail.com>
2022-10-04 17:16:10 +0100

    Pushing my dotfiles to github

---
+++ lua/lenti/packer.lua
@@ -0,0 +1,26 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+  use 'ryanoasis/vim-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { "williamboman/mason.nvim" }
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+end)
---
+++ lua/lenti/remap.lua
@@ -0,0 +1,13 @@
+local nnoremap = require("lenti.keymap").nnoremap
+local builtin = require("telescope.builtin")
+
+-- This is for netwr directory list
+nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
+-- Telescope
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+-- Mason
+nnoremap("<leader>ma", "<cmd>Mason<CR>")
