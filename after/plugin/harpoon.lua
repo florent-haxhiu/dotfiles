034d1e3ed162de31e5251aeca0f4a4e4bdf3bac7
florent-haxhiu <florenthaxhiu03@gmail.com>
2022-10-21 11:33:10 +0100

    Changed the layout

--- after/plugin/harpoon.lua
+++
@@ -1,6 +0,0 @@
-local nnoremap = require("lenti.keymap").nnoremap
-
-local silent = { silent = true }
-
-nnoremap("<leader>ar", function() require("harpoon.mark").add_file() end, silent)
-nnoremap("<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
--- after/plugin/lualine.lua
+++
@@ -1,8 +0,0 @@
-local auto = require'lualine.themes.auto'
-
-require('lualine').setup {
-    options = {
-        theme = auto,
-        component_separators = '|',
-    }
-}
