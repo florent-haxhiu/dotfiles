01fc846ab7d117349ecf268b3a62afe0bb00e5fd
florent-haxhiu <florenthaxhiu03@gmail.com>
2022-11-06 16:51:38 +0000

    Installed starship, need to configure a bit more. Changed kitty font to Fira Code Nerd Font

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  bash -c "fm6000 -c 'random'"
+    bash -c "fm6000 -c 'random'"
     #fzf_configure_bindings --directory=\cf
     #starship init fish | source
+    starship init fish | source
 
-  # Shortcut for nvim, can't be asked to type out nvim -> vim is easier
-  function vim
-    nvim $argv
-  end 
+# Shortcut for nvim, can't be asked to type out nvim -> vim is easier
+    function vim
+        nvim $argv
+    end
 
-  function python
-    python3 $argv
-  end
+    function python
+        python3 $argv
+    end
 
 end
