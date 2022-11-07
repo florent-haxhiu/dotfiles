a6ae59a5c6905ee242b8620453480114d3b5e486
florent-haxhiu <florenthaxhiu03@gmail.com>
2022-11-07 07:54:52 +0000

    Added nvm and starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bash -c "fm6000 -c 'random'"
-    #fzf_configure_bindings --directory=\cf
-    #starship init fish | source
+
     starship init fish | source
 
-# Shortcut for nvim, can't be asked to type out nvim -> vim is easier
+    # Shortcut for nvim, can't be asked to type out nvim -> vim is easier
     function vim
         nvim $argv
     end
