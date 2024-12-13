00ab5422fc8bfd0848a19cfd964a4e039ed37ac8
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-12-13 20:45:44 +0000

    add: vimtex for maths

--- init.lua
+++ init.lua
@@ -1,19 +1,20 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ Setting options ]]
-require 'options'
+vim.cmd("source /home/lenti/.config/nvim/lua/options.vim")
+require("options")
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require("keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require("lazy-plugins")
 
-require 'custom.after'
+require("custom.after")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
